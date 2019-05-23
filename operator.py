eb3d72e39873e1a082e16dd4a2ca6b6c3c2978cb
tropiano4 <tropiano.4@osu.edu>
2019-05-23 12:17:44 -0600

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,14 @@
+# Created 05/03/19 by A.T. (tropiano.4@osu.edu)
+
+# Operator code: ...
+
+
+import numpy as np
+import numpy.linalg as la
+from scipy.special import spherical_jn
+
+
+class Operator(object):
+    
+    def __init__(self):
+        
