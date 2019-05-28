00eab95ddee902397bce639ccdfd7a4b7c75972c
tropiano4 <tropiano.4@osu.edu>
2019-05-28 11:16:51 -0400

    Update srg_unitary_transformation.py

--- SRG_codes/srg_unitary_transformation.py
+++ SRG_codes/srg_unitary_transformation.py
@@ -1,57 +1,78 @@
-# Created 05/03/19 by A.T. (tropiano.4@osu.edu)
+#!/usr/bin/env python3
 
-# SRG unitary transformation function: this function returns an SRG unitary
-# transformation given an initial and SRG evolved Hamiltonian in units MeV.
+#------------------------------------------------------------------------------
+# File: srg_unitary_transformation.py
+#
+# Author:   A. J. Tropiano (tropiano.4@osu.edu)
+# Date:     May 3, 2019
+# 
+# SRG unitary transformation function returns an SRG unitary transformation 
+# given an initial and SRG evolved Hamiltonian in units MeV.
+#
+#------------------------------------------------------------------------------
 
 
 import numpy as np
 import numpy.linalg as la
 
 
-def SRG_unitary_transformation(H0_matrix, Hs_matrix):
-    '''Returns an SRG unitary transformation built out of eigenvectors of the
-    initial and evolved matrices.'''
+def SRG_unitary_transformation(H_initial, H_evolved):
+    """
+    SRG unitary transformation built out of eigenvectors of the initial and 
+    evolved Hamiltonian matrices.
     
-    # Arguments
+    Parameters
+    ----------
+    H_initial : 2-D ndarray
+        Initial Hamiltonian matrix in units MeV.
+    H_evolved : 2-D ndarray
+        Evolved Hamiltonian matrix in units MeV.
+        
+    Returns
+    -------
+    U : 2-D ndarray
+        SRG unitary transformation matrix (unitless).
+        
+    """
     
-    # H0_matrix (2-D NumPy array): Initial Hamiltonian in units MeV
-    # Hs_matrix (2-D NumPy array): Evolved Hamiltonian in units MeV
+    # Length of the matrices
+    N = len(H_initial)
     
-    # Dimension of the matrices
-    N = len(H0_matrix)
-    
-    # Diagonalize unevolved Hamiltonian
-    eig_unevolved, vecs_unevolved = la.eig(H0_matrix)
-    # Diagonalize evolved Hamiltonian
-    eig_evolved, vecs_evolved = la.eig(Hs_matrix)
+    # Diagonalize the initial Hamiltonian
+    eig_initial, vecs_initial = la.eig(H_initial)
+    # Diagonalize the evolved Hamiltonian
+    eig_evolved, vecs_evolved = la.eig(H_evolved)
     
     # Store eigenvalue and eigenvector pairs in a dictionary which will sort by 
     # lowest eigenvalue naturally
-    d_unevolved = {}
+    d_initial = {}
     d_evolved = {}
+    
     for i in range(N):
-        d_unevolved[eig_unevolved[i]] = vecs_unevolved[:,i]
-        d_evolved[eig_evolved[i]] = vecs_evolved[:,i]
+        
+        d_initial[ eig_initial[i] ] = vecs_initial[:, i]
+        d_evolved[ eig_evolved[i] ] = vecs_evolved[:, i]
         
     # Sort eigenvalues from lowest to highest energy
-    eig_unevolved = np.sort(eig_unevolved)
+    eig_initial = np.sort(eig_initial)
     eig_evolved = np.sort(eig_evolved)
 
     # Initialize unitary transformation U
-    U = np.zeros((N,N))
-    # Unitary transformation is given by summing over outer product of evolved 
-    # and unevolved eigenvectors
+    U = np.zeros( (N, N) )
+    
+    # The unitary transformation is given by summing over the outer product of 
+    # evolved and initial eigenvectors
     for alpha in range(N):
         
         # Eigenvectors
-        psi_alpha_unevolved = d_unevolved[eig_unevolved[alpha]]
-        psi_alpha_evolved = d_evolved[eig_evolved[alpha]]
+        psi_alpha_initial = d_initial[ eig_initial[alpha] ]
+        psi_alpha_evolved = d_evolved[ eig_evolved[alpha] ]
         
-        # Make sure the phases match using dot-product
-        if psi_alpha_unevolved.T @ psi_alpha_evolved < 0:
+        # Make sure the phases match using dot product
+        if psi_alpha_initial.T @ psi_alpha_evolved < 0:
             psi_alpha_evolved = -psi_alpha_evolved
         
         # Outer product of eigenvectors
-        U += np.outer(psi_alpha_evolved,psi_alpha_unevolved)
+        U += np.outer(psi_alpha_evolved, psi_alpha_initial)
         
     return U
