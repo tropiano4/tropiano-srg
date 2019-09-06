9c5b8c71705a3acd092e6eb6554bce8e979f0f9a
tropiano4 <tropiano.4@osu.edu>
2019-09-06 16:12:23 -0400

    Generating NN potentials in other partial wave channels
    1S0, 1P1, and 3P0 for EM N3LO (500 MeV), RKE N3LO (450 MeV), and Gezerlis N2LO (1 fm) up to lambda 1 fm^-1 with Wegner and block-diagonal (1, 2 fm^-1) generators.

--- Potentials/vsrg_macos/run_generate_vsrg_vlowk.pl
+++ Potentials/vsrg_macos/run_generate_vsrg_vlowk.pl
@@ -51,12 +51,12 @@ $rsmooth = 0.00;  # real smoothness parameter [for non-exponential] (0.0 for SRG
 $nmod = 54;  # momentum points for low-k = P space (up to lambda for SRG)
 #$nmod = 74; # changes made by AT to test SRG evolution of momentum projection operator
 #$ntot = 100; # setting in Anderson:2010aq
-#$ntot = 120; # momentum points for P+Q = full = bare space
+$ntot = 120; # momentum points for P+Q = full = bare space
 #$ntot = 121;
 #$ntot = 140; # changes made by AT
-$ntot = 141;
-$kmax = 6.0;
-#$kmax = 8.0; # kmax ==> maximum momentum for bare potential   # following the sample code by Evgeny 30-Nov-2017
+#$ntot = 141;
+#$kmax = 6.0;
+$kmax = 8.0; # kmax ==> maximum momentum for bare potential   # following the sample code by Evgeny 30-Nov-2017
 #$kmax = 30.0;
 $kmid = 2.0;  # mesh goes from 0 to kmid, then kmid to kmax
 #$kmid = 3.0; # setting in Anderson:2010aq
@@ -94,11 +94,11 @@ $kmid = 2.0;  # mesh goes from 0 to kmid, then kmid to kmax
 #     cutnum = 1,2,3,4--> 400, 450, 500, 550 MeV cutoff
 
 #@kvnns = (06); # Argonne v18
-@kvnns = (10); # Entem-Machleidt
+#@kvnns = (10); # Entem-Machleidt
 #@kvnns = (32); # Epelbaum
 #@kvnns = (90,91,92,93,95,96,97,98,100,101,102,103,105,106,107,108,110,111,112,113,115,116,117,118); # RKE semi-local potentials
 #@kvnns = (90); # RKE semi-local potential at LO with specified cutoff (unless changed it's 400 MeV)
-#@kvnns = (105,106,107,110,111,112); # RKE semi-local potential at N3LO and N4LO
+@kvnns = (105,106,107,110,111,112); # RKE semi-local potential at N3LO and N4LO
 
 # The following list is all of the semi-local EKM potentials
 #@kvnns = (40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64);
@@ -148,7 +148,8 @@ $J_max = 13;  # highest J value
         "3O11","1O11","3O12",
         "3Q12","1Q12","3Q13"
         );
-@pws = ("1S0","3S1");
+#@pws = ("1S0","3S1");
+@pws = ("1P1", "3P0");
 #@pws = ("3P0");
 #@pws = ("3N12","1M10","1O12");
 
