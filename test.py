311a7be2d3f1a122867e8a9d913643af9a6ac5af
tropiano4 <tropiano.4@osu.edu>
2019-09-23 16:43:50 -0400

    Update test.py
    Ran SRG-evolution on several potentials.

--- test.py
+++ test.py
@@ -30,17 +30,28 @@ import numpy as np
 from run_srg import run_srg
 
 
-kvnn = 902
+kvnn_list = [10, 106]
 # Could add the RKE 1-20 fm^-1 LO potentials as 903, 904, ...
-channel = '3S1'
-kmax = 30.0
-kmid = 4.0
+channel_list = ['1S0', '3S1', '1P1']
+kmax = 10.0
+kmid = 2.0
 ntot = 120
 
-lambda_array = np.array( [6.0, 3.0, 2.0, 1.5, 1.0] )
-generator_list = ['T']
+lambda_array = np.array( (6.0, 3.0, 2.0, 1.5, 1.0) )
+generator_list = ['Wegner', 'Block-diag']
 
-for generator in generator_list:
+for kvnn in kvnn_list:
+    for channel in channel_list:
+        for generator in generator_list:
     
-    d = run_srg(kvnn, channel, kmax, kmid, ntot, generator, lambda_array,
-                save=True)
+            if generator == 'Wegner':
+            
+                d = run_srg(kvnn, channel, kmax, kmid, ntot, generator,
+                            lambda_array, save=True)
+                
+            else:
+        
+                for lamb in lambda_array:
+            
+                    d = run_srg(kvnn, channel, kmax, kmid, ntot, generator,
+                                lambda_array, lambda_bd=lamb, save=True)
