e027180667488aa154deed11a87e0470b6ad1d07
tropiano4 <tropiano.4@osu.edu>
2019-09-24 10:45:36 -0400

    Generated EM N3LO, RKE N3LO, RKE N4LO potentials
    Fixed the mesh to match Gezerlis et al. local potential: kmax = 10, kmid = 2 fm^-1 with nmod = 60 and ntot = 120.

--- Potentials/vsrg_macos/run_generate_vsrg_vlowk.pl
+++ Potentials/vsrg_macos/run_generate_vsrg_vlowk.pl
@@ -48,7 +48,8 @@ $nsmooth = 0;     # integer smoothness parameter [exponent] (0 for SRG)
 $rsmooth = 0.00;  # real smoothness parameter [for non-exponential] (0.0 for SRG)
 
 # Set the momentum mesh parameters
-$nmod = 54;  # momentum points for low-k = P space (up to lambda for SRG)
+#$nmod = 54;  # momentum points for low-k = P space (up to lambda for SRG)
+$nmod = 60;
 #$nmod = 74; # changes made by AT to test SRG evolution of momentum projection operator
 #$ntot = 100; # setting in Anderson:2010aq
 $ntot = 120; # momentum points for P+Q = full = bare space
@@ -95,11 +96,11 @@ $kmid = 2.0;  # mesh goes from 0 to kmid, then kmid to kmax
 #     cutnum = 1,2,3,4--> 400, 450, 500, 550 MeV cutoff
 
 #@kvnns = (06); # Argonne v18
-@kvnns = (10); # Entem-Machleidt
+#@kvnns = (10); # Entem-Machleidt
 #@kvnns = (32); # Epelbaum
 #@kvnns = (90,91,92,93,95,96,97,98,100,101,102,103,105,106,107,108,110,111,112,113,115,116,117,118); # RKE semi-local potentials
 #@kvnns = (90); # RKE semi-local potential at LO with specified cutoff (unless changed it's 400 MeV)
-#@kvnns = (105,106,107,110,111,112); # RKE semi-local potential at N3LO and N4LO
+@kvnns = (105,106,107,110,111,112); # RKE semi-local potential at N3LO and N4LO
 
 # The following list is all of the semi-local EKM potentials
 #@kvnns = (40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64);
