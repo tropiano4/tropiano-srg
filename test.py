f81f109718289e80be79b9ee4811b7c12b961e21
tropiano4 <tropiano.4@osu.edu>
2019-09-24 16:55:24 -0400

    Update test.py
    Comparing wave functions from different SRG-evolved potentials by looking at momentum distribution functions.

--- test.py
+++ test.py
@@ -21,36 +21,106 @@
 #                momentum_projection_operator_testv1.py in Old_codes based off
 #                last tests in this script.
 #   09/10/19 --- Using this script to run SRG evolution on several potentials.
-#
+#   09/24/19 --- Comparing wave functions from different SRG-evolved potentials
+#                by looking at momentum distribution functions.
 #------------------------------------------------------------------------------
 
 
+import matplotlib.pyplot as plt
 import numpy as np
 # Scripts made by A.T.
-from run_srg import run_srg
+from Potentials.vsrg_macos import load_save_potentials as lp
+import observables as ob
+from SRG_codes.srg_unitary_transformation import SRG_unitary_transformation
 
 
+# --- Set up --- #
+
+# Potential details
 kvnn_list = [10, 106]
-channel_list = ['1S0', '3S1', '1P1']
+channel = '3S1'
 kmax = 10.0
 kmid = 2.0
 ntot = 120
 
-lambda_array = np.array( (6.0, 3.0, 2.0, 1.5, 1.0) )
-generator_list = ['Wegner', 'Block-diag']
+# SRG details
+generator = 'Wegner'
+lamb = 1.5
 
+# Deuteron energy in MeV
+eps = -2.22
+
+
+# --- Load wave functions, calculate momentum distribution, and store --- #
+
+d = {}
+d['init'] = {}
+d['srg'] = {}
 for kvnn in kvnn_list:
-    for channel in channel_list:
-        for generator in generator_list:
     
-            if generator == 'Wegner':
-            
-                d = run_srg(kvnn, channel, kmax, kmid, ntot, generator,
-                            lambda_array, save=True)
-                
-            else:
+    # Load momentum and weights
+    k_array, k_weights = lp.load_momentum(kvnn, channel, kmax, kmid, ntot)
+    
+    # Load initial Hamiltonian
+    H_initial = lp.load_hamiltonian(kvnn, channel, kmax, kmid, ntot)
+    
+    # Compute initial wave function
+    psi_initial = ob.wave_function(H_initial, eps)
+    u_initial = psi_initial[:ntot] # 3S1 component
+    w_initial = psi_initial[ntot:] # 3D1 component
+    
+    # Initial momentum distribution (divide by momenta and weights for 
+    # mesh-independent result)
+    d['init'][kvnn] = ( u_initial**2 + w_initial**2 ) / \
+                         ( k_array**2 * k_weights )
+    
+    # Build unitary transformation
+    H_evolved = lp.load_hamiltonian(kvnn, channel, kmax, kmid, ntot, 'srg',
+                                    generator, lamb)
+    U_matrix = SRG_unitary_transformation(H_initial, H_evolved)
         
-                for lamb in lambda_array:
-            
-                    d = run_srg(kvnn, channel, kmax, kmid, ntot, generator,
-                                lambda_array, lambda_bd=lamb, save=True)
+    # Compute evolved wave function
+    psi_evolved = ob.wave_function(H_initial, eps, U=U_matrix)
+    u_evolved = psi_evolved[:ntot] # 3S1 component
+    w_evolved = psi_evolved[ntot:] # 3D1 component
+        
+    # Evolved momentum distribution (divide by momenta and weights for 
+    # mesh-independent result)
+    d['srg'][kvnn] = ( u_evolved**2 + w_evolved**2 ) / \
+                     ( k_array**2 * k_weights )
+    
+# Calculate ratio and absolute difference
+diff_init = abs( d['init'][kvnn_list[0]] - d['init'][kvnn_list[1]] )
+ratio_init = d['init'][kvnn_list[0]] / d['init'][kvnn_list[1]]             
+diff_srg = abs( d['srg'][kvnn_list[0]] - d['srg'][kvnn_list[1]] )
+ratio_srg = d['srg'][kvnn_list[0]] / d['srg'][kvnn_list[1]]
+
+
+# --- Plot --- #
+
+# Absolute difference
+plt.clf()
+plt.semilogy(k_array, diff_init, color='xkcd:blue', label='Initial')
+plt.semilogy(k_array, diff_srg, color='xkcd:red', label='SRG')
+plt.xlim( (0.0, 3.0) )
+plt.ylim( (1e-9, 1e0) )
+plt.title('Absolute difference in '+r'$|\phi_d(k)|^2$')
+plt.xlabel('k [fm' + r'$^{-1}$' + ']')
+plt.ylabel('EM - RKE [fm' + r'$^3$' + ']')
+plt.legend()
+plt.show()
+
+# Ratio
+plt.clf()
+plt.plot(k_array, ratio_init, color='xkcd:blue', label='Initial')
+plt.plot(k_array, ratio_srg, color='xkcd:red', label='SRG')
+plt.plot(k_array, np.ones(ntot), 'k:')
+plt.xlim( (0.0, 3.0) )
+plt.ylim( (0.0, 3.0) )
+#plt.xlim( (0.0, 10.0) )
+#plt.ylim( (0.0, 10.0) )
+plt.title('Ratio of '+r'$|\phi_d(k)|^2$')
+plt.xlabel('k [fm' + r'$^{-1}$' + ']')
+plt.ylabel('EM / RKE')
+plt.legend()
+plt.show()
