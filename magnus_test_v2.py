5a5e2c73e39e9e6890f33069c2d86f6e2f3858ed
tropiano4 <tropiano.4@osu.edu>
2020-01-27 16:49:55 -0500

    Further testing of convergence in Magnus expansion
    Looking at difference between Wegner and block-diagonal generator.

---
+++ magnus_test_v2.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+
+#------------------------------------------------------------------------------
+# File: magnus_test_v1.py
+#
+# Author:   A. J. Tropiano (tropiano.4@osu.edu)
+# Date:     January 24, 2019
+# 
+# Test how the Magnus expansion converges looking at the norm of \eta(s) and
+# \Omega(s).
+#
+#------------------------------------------------------------------------------
+
+
+from os import chdir, getcwd
+from math import factorial
+#from matplotlib.offsetbox import AnchoredText
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import linalg as la
+from scipy.linalg import expm
+from sympy import bernoulli
+import time
+# Scripts made by A.T.
+from Figures import figures_functions as ff
+from Potentials.vsrg_macos import load_save_potentials as lp
+
+
+class Magnus_test(object):
+    
+    
+    def __init__(self, k_array, H_initial, generator, k_magnus, ds, 
+                 lambda_bd=0.00, coupled_channel=True):
+        
+        # Convert Hamiltonian and relative kinetic energy to scattering units
+        hbar_sq_over_M = 41.47
+        H_initial /= hbar_sq_over_M
+        self.H_initial = H_initial
+        self.N = len(H_initial)
+        self.generator = generator
+        self.k_magnus = k_magnus
+        self.ds = ds
+        
+        # Initialize factorial and Bernoulli number arrays for summations
+        factorial_array = np.zeros(k_magnus+1)
+        magnus_factors = np.zeros(k_magnus+1)
+        for i in range(k_magnus+1):
+            factorial_array[i] = factorial(i)
+            magnus_factors[i] = bernoulli(i) / factorial(i)
+        self.factorial_array = factorial_array
+        self.magnus_factors = magnus_factors
+        
+        # Set-up for block-diagonal generator
+        if generator == 'Block-diag':
+            
+            # Array of True and False values
+            # In index notation: bool_array[i] = k_array[i] < lambda_bd[i]
+            bool_array = k_array < lambda_bd
+        
+            # Length of momentum array
+            n = len(k_array)
+        
+            # Build projection operators using bool_array
+        
+            # Matrix of ones along the diagonal up to k > lambda_bd
+            p = np.diag( np.ones(n) * bool_array )
+        
+            # Opposite of p
+            q = np.identity(n) - p
+        
+            # Projection operators for coupled channel potentials
+            if coupled_channel:
+                o = np.zeros((n,n))
+                self.P = np.vstack( ( np.hstack( (p, o) ), 
+                                      np.hstack( (o, p) ) ) )
+                self.Q = np.vstack( ( np.hstack( (q, o) ), 
+                                      np.hstack( (o, q) ) ) )
+
+            else:
+                self.P = p
+                self.Q = q
+
+
+    def commutator(self, A, B):
+
+        return A @ B - B @ A
+
+    
+    def derivative(self, O_evolved):
+        
+        # Compute the evolving Hamiltonian with the BCH formula
+        #H_evolved = self.bch_formula(self.H_initial, O_evolved, 25)
+        # Use expm instead
+        H_evolved = expm(O_evolved) @ self.H_initial @ expm(-O_evolved)
+
+        # Wegner SRG generator, eta = [G,H] where G = H_D (diagonal of the 
+        # evolving Hamiltonian)
+        if self.generator == 'Wegner':
+            G = np.diag( np.diag(H_evolved) )
+        else:
+            G = self.P @ H_evolved @ self.P + self.Q @ H_evolved @ self.Q
+        eta = self.commutator( G, H_evolved)
+        eta_norm = la.norm(eta)
+        
+        # Initial nested commutator ad_Omega^0(eta)
+        ad = eta
+        
+        # k = 0 term
+        dO_matrix = self.magnus_factors[0] * ad
+        
+        # Sum from k = 1 to k = k_magnus
+        for k in range(1, self.k_magnus + 1):
+            
+            ad = self.commutator(O_evolved, ad)
+            dO_matrix += self.magnus_factors[k] * ad
+    
+        return dO_matrix, eta_norm
+
+
+    def euler_method(self, lamb):
+        
+        # Set initial s value and initial value of omega
+        s = 0.0
+        s_final = 1.0 / lamb**4.0
+        ds = self.ds
+        s_array = np.arange(0.0, s_final, ds)
+        n = len(s_array)
+        eta_array = np.zeros(n+1)
+        omega_array = np.zeros(n+1)
+        
+        # Initial Magnus omega matrix
+        O_evolved = np.zeros( (self.N, self.N) )
+
+        # Start time
+        print('Starting...')
+        t0 = time.time() # Start time
+        
+        # Step in s until s_final is reached
+        i = 0
+        for s in s_array:
+
+            # Next step in s
+            try:
+                dO, eta_norm = self.derivative(O_evolved)
+                O_evolved += dO * ds
+                eta_array[i] = eta_norm
+                omega_array[i] = la.norm(O_evolved)
+            # Check for OverflowError (this occurs when omega grows to infinity)
+            except OverflowError:
+                print('_'*85)
+                print('Infinities or NaNs encountered in omega matrix.')
+                print('Try using a smaller step-size')
+                # Store in dictionary
+                d = {}
+                d['s_array'] = s_array[:i]
+                d['eta_array'] = eta_array[:i]
+                d['omega_array'] = omega_array[:i]
+                return d
+            except ValueError:
+                print('_'*85)
+                print('Infinities or NaNs encountered in omega matrix.')
+                print('Try using a smaller step-size')
+                # Store in dictionary
+                d = {}
+                d['s_array'] = s_array[:i]
+                d['eta_array'] = eta_array[:i]
+                d['omega_array'] = omega_array[:i]
+                return d
+            
+            i += 1
+                
+        # Step to last s value
+        ds_exact = s_final - s_array[-1]
+
+        dO, eta_norm = self.derivative(O_evolved)
+        O_evolved += dO * ds_exact
+        eta_array[i] = eta_norm
+        omega_array[i] = la.norm(O_evolved)
+        s_array = np.append(s_array, s_final)
+        # Evolve Hamiltonian
+        H_evolved = expm(O_evolved) @ self.H_initial @ expm(-O_evolved)
+
+        t1 = time.time() # End time
+        mins = round( (t1 - t0) / 60.0, 2)
+        print( 'H(s) done evolving to final lambda = %.1f fm^-1 after %f mins'
+              % (lamb, mins) )
+        
+        # Store in dictionary
+        d = {}
+        d['H'] = H_evolved
+        d['s_array'] = s_array
+        d['eta_array'] = eta_array
+        d['omega_array'] = omega_array
+        
+        return d
+
+
+# Make executable
+if __name__ == '__main__':
+    
+    
+    # --- Set-up --- #
+    
+    cwd = getcwd()
+    hbar_sq_over_M = 41.47
+    kvnn = 10
+    #kvnn = 79
+    #kvnn = 111
+    channel = '3S1'
+    ntot = 120
+    generator_list = ['Wegner', 'Block-diag']
+    lamb = 1.5
+    k_magnus = 6
+    
+    # Mesh and evolution specifications
+    kmax = 30.0
+    kmid = 4.0
+    ds = 1e-5
+    
+    # Load Hamiltonian and momentum
+    H_initial = lp.load_hamiltonian(kvnn, channel, kmax, kmid, ntot)
+    k_array, _ = lp.load_momentum(kvnn, channel, kmax, kmid, ntot)
+    
+    # --- Evolve and plot relevant figures --- #
+    
+    plt.close('all')
+    row = 1
+    col = 2
+    f, (ax1, ax2) = plt.subplots(row, col, figsize=(4*col, 4*row))
+    
+    # Labels, fontsize, and location
+    x_label = 's'
+    x_label_size = 18
+    y_label_1 = r'$||\eta(s)||$'
+    y_label_2 = r'$||\Omega(s)||$'
+    y_label_size = 20
+    legend_label_size = 12
+    
+    for generator in generator_list:
+        
+        if generator == 'Block-diag':
+            magnus = Magnus_test(k_array, H_initial, generator, k_magnus, ds, 
+                                 lambda_bd=lamb)
+            curve_color = ff.xkcd_colors(1)
+            curve_style = ff.line_styles(1)
+        else:
+            magnus = Magnus_test(k_array, H_initial, generator, k_magnus, ds)
+            curve_color = ff.xkcd_colors(2)
+            curve_style = ff.line_styles(0)
+        d = magnus.euler_method(lamb)
+
+        generator_label = ff.generator_label_conversion(generator)
+        
+        ax1.loglog(d['s_array'], d['eta_array'], color=curve_color, 
+                   linestyle=curve_style, label=generator_label)
+        ax2.loglog(d['s_array'], d['omega_array'], color=curve_color,
+                   linestyle=curve_style, label=generator_label)
+    
+    # Axes limits
+    ax1.set_xlim([1e-5, 1e0])
+    ax2.set_xlim([1e-5, 1e0])
+    ax1.set_ylim([1e-4, 1e4])
+    ax2.set_ylim([1e-6, 1e2])
+    # Axes labels
+    ax1.set_xlabel(x_label, fontsize=x_label_size)
+    ax1.set_ylabel(y_label_1, fontsize=y_label_size)
+    ax2.set_xlabel(x_label, fontsize=x_label_size)
+    ax2.set_ylabel(y_label_2, fontsize=y_label_size)
+    # Add legend
+    ax1.legend(loc='upper right', frameon=False, fontsize=legend_label_size)
+    ax2.legend(loc='upper right', frameon=False, fontsize=legend_label_size)
+    f.tight_layout()
+
+    file_name = 'eta_omega_norms_kvnn_%d_generators_%s_%s_%s_k_magnus_%d' % \
+                (kvnn, channel, generator_list[0], generator_list[1], k_magnus)
+
+    # Save figure
+    chdir('Figures/Operator_evolution')
+    f.savefig(file_name+'.pdf', bbox_inches='tight')
+    chdir(cwd)
