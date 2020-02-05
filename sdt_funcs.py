6008e9d79c1f75d6d3e236fe55cbbdf5166bee06
tropiano4 <tropiano.4@osu.edu>
2020-02-05 14:43:31 -0500

    Update sdt_funcs.py
    Added comments to this script.

--- sdt_funcs.py
+++ sdt_funcs.py
@@ -6,9 +6,9 @@
 # Author:   A. J. Tropiano (tropiano.4@osu.edu)
 # Date:     January 28, 2019
 # 
-# Description of program.
-# https://arxiv.org/pdf/1308.5963.pdf
-# https://arxiv.org/pdf/nucl-th/0703076.pdf
+# Functions for comparing two Hamiltonians or potentials based off spectral
+# distribution theory (SDT). See https://arxiv.org/pdf/1308.5963.pdf and
+# https://arxiv.org/pdf/nucl-th/0703076.pdf for more details.
 #
 #------------------------------------------------------------------------------
 
@@ -18,14 +18,18 @@ import numpy as np
 
 def expectation_value(A):
     """
-    Description of function.
+    Calculates the expectation value of A with dimensionality N. Related to
+    the trace of the operator.
     
     Parameters
     ----------
-    
+    A : 2-D ndarray
+        Input matrix.
     
     Returns
     -------
+    output : float
+        Expectation value of A.
     
     """
     
@@ -34,17 +38,21 @@ def expectation_value(A):
 
 def sigma(A):
     """
-    Description of function.
+    Calculates the positive square root of the variance of A.
     
     Parameters
     ----------
-    
+    A : 2-D ndarray
+        Input matrix.
     
     Returns
     -------
+    output : float
+        Square root of the variance of A.
     
     """
     
+    # A^2
     A2 = A @ A
     
     return np.sqrt( expectation_value(A2) - expectation_value(A)**2 )
@@ -52,33 +60,43 @@ def sigma(A):
 
 def inner_product(A, B):
     """
-    Description of function.
+    Calculates the inner product of two matrices in SDT.
     
     Parameters
     ----------
-    
+    A : 2-D ndarray
+        First input matrix.
+    B : 2-D ndarray
+        Second input matrix.
     
     Returns
     -------
+    output : float
+        Inner product of A and B.
     
     """
     
+    # C is the matrix product of A^{\dagger} B (we're assuming A is real)
     C = A.T @ B
-    
     
     return expectation_value(C) - expectation_value(A.T) * expectation_value(B)
 
 
 def correlation_coefficient(A, B):
     """
-    Description of function.
+    Calculates the correlation coefficient between two matrices in SDT.
     
     Parameters
     ----------
-    
+    A : 2-D ndarray
+        First input matrix.
+    B : 2-D ndarray
+        Second input matrix.
     
     Returns
     -------
+    output: float
+        Correlation coefficient.
     
     """
     
