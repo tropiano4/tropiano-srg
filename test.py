600b0943d8f93a36c4ab6a7bb70482d2539c3837
tropiano4 <tropiano.4@osu.edu>
2020-02-11 16:58:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -33,125 +33,137 @@
 #                and block-diagonal SRG.
 #   10/29/19 --- Testing r^2 operator and RMS half-radius of deuteron.
 #   01/03/20 --- Looking at block-diagonal unitary transformations.
+#   02/11/20 --- Trying to understand SRG induced terms in momentum projection
+#                operator by plotting some terms.
 #
 #------------------------------------------------------------------------------
 
 
 from matplotlib.offsetbox import AnchoredText
-import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import numpy as np
 # Scripts made by A.T.
 from Figures import figures_functions as ff
 from Potentials.vsrg_macos import load_save_potentials as lp
-import observables as ob
-#import operators as op
-from SRG_codes.srg_unitary_transformation import SRG_unitary_transformation
+import operators as op
 
 
+# --- Set-up --- #
+
+# Potential specifications
 kvnn = 111
-#kvnn = 79
-#channel = '1P1'
-#channel = '3S1'
-channel = '1S0'
+channel = '3S1'
 kmax = 10.0
 kmid = 2.0
 ntot = 120
 
-k_array, k_weights = lp.load_momentum(kvnn, channel, kmax, kmid, ntot)
-H_initial = lp.load_hamiltonian(kvnn, channel, kmax, kmid, ntot)
+# SRG specifications
+generator = 'Wegner'
+lamb = 1.5
 
-generator = 'Block-diag'
-#generator = 'Wegner'
-lamb = 1.0
-#lamb = 1.2
-#lamb = 1.5
-#lamb = 2.0
-lambda_bd = 2.0
+# Load momentum and potential
+k_array, _ = lp.load_momentum(kvnn, channel, kmax, kmid, ntot)
+V_matrix = lp.load_potential(kvnn, channel, kmax, kmid, ntot, 'srg', 
+                             generator, lamb)
 
-H_evolved = lp.load_hamiltonian(kvnn, channel, kmax, kmid, ntot, 'srg', 
-                                generator, lamb, lambda_bd)
-U_matrix = SRG_unitary_transformation(H_initial, H_evolved)
+# --- Main calculation --- #
 
-if channel == '1P1' or channel == '1S0':
-    I = np.eye(ntot, ntot)
-elif channel == '3S1':
-    I = np.eye(2*ntot, 2*ntot)
+# Set q-value and take slices of potential correspondin
+#q = 0.3
+q = 3.0
+q_index = op.find_q_index(q, k_array)
 
-if channel == '3S1':
-    factor_array = np.concatenate((np.sqrt(k_weights), np.sqrt(k_weights)))
-    #factor_array = np.concatenate((k_array*np.sqrt(k_weights), 
-                                   #k_array*np.sqrt(k_weights)))
+# Calculate induced contribution
+if q == 3.0:
+    row, col = np.meshgrid( V_matrix[:ntot, q_index], V_matrix[q_index, :ntot] )
+    induced_cont = 2 * q**4 * row * col
 else:
-    factor_array = np.sqrt(k_weights)
+    row, col = np.meshgrid( k_array**2 * V_matrix[:ntot, q_index], 
+                            k_array**2 * V_matrix[q_index, :ntot] )
+    induced_cont = 2 * row * col
 
-row, col = np.meshgrid(factor_array, factor_array)
+# Interpolate
+k_array_int, induced_cont_int = ff.interpolate_matrix(k_array, induced_cont, 
+                                                      4.0)
 
-delta_U = (U_matrix - I) / row / col
 
+# --- Plot figure --- #
+
+# Size of figure
+row_number = 1
+col_number = 1
+figure_size = (4*col_number, 3.5*row_number) # extra width for colorbar
+
+# Limits of x and y axes
+axes_max = 4.0
+        
+# Specifications of axes
+# Step-size in labeling tick marks
+axes_stepsize = 1.0
+# x and y axes ticks
+axes_ticks = np.arange(0.0, axes_max + axes_stepsize, axes_stepsize)
+        
+# Labels and fontsize
 x_label = "k' [fm" + r'$^{-1}$' + ']'
 y_label = 'k [fm' + r'$^{-1}$' + ']'
-mx = 1e-1
-mn = -1e-1
+axes_label_size = 18
+anchored_text_label = r'$\lambda=%.1f$' % lamb + '\nq=%.1f' % q
+anchored_text_size = 17
+axes_tick_size = 18
+colorbar_tick_size = 18
+    
+# Color scheme for contour plots
 color_style = 'jet'
 
-f, ax = plt.subplots(1, 1, figsize=(4, 3.5))
-if channel == '1P1' or channel == '1S0':
-    c = ax.pcolormesh(k_array, k_array, delta_U, cmap=color_style, vmin=mn, 
-                      vmax=mx)
-elif channel == '3S1':
-    c = ax.pcolormesh(k_array, k_array, delta_U[:ntot,:ntot], cmap=color_style, 
-                      vmin=mn, vmax=mx)
+# Location of labels
+
+
+# Things which depend on the q value: limits of colorbar and label location
+if q < axes_max/2:
+    mx = 5.0
+    mn = -5.0
+    anchored_text_location = 'lower right'
+else:
+    mx = 1.2
+    mn = -1.2
+    anchored_text_location = 'upper left'
+    
+levels = np.linspace(mn, mx, 41)
+    
+
+plt.close('all')
+f, ax = plt.subplots(row_number, col_number, figsize=figure_size)
+
+c = ax.contourf(k_array_int, k_array_int, induced_cont_int, levels, 
+                cmap=color_style)
+# Specify axes limits
+ax.set_xlim( (0, axes_max) )
+ax.set_ylim( (0, axes_max) )
+# Specify axes tick marks
+ax.xaxis.set_ticks(axes_ticks)
+ax.xaxis.set_ticklabels(axes_ticks)
+ax.yaxis.set_ticks(axes_ticks)
+ax.yaxis.set_ticklabels(axes_ticks)
+# Position of x-axis label and tick marks
 ax.xaxis.set_label_position('top')
 ax.xaxis.tick_top()
-ax.tick_params(labeltop=True)
-ax.set_xlabel(x_label)
-ax.set_ylabel(y_label)
-channel_anchored_text = AnchoredText(ff.channel_label_conversion(channel),
-                                     loc='lower right')
-ax.add_artist(channel_anchored_text)
-plt.gca().invert_yaxis()
+ax.tick_params(labeltop=True, labelsize=axes_tick_size)
+# Set axes labels
+ax.set_xlabel(x_label, fontsize=axes_label_size)
+ax.set_ylabel(y_label, fontsize=axes_label_size)
+# Add lambda label as anchored text
+anchored_text = AnchoredText(anchored_text_label, 
+                             prop=dict(size=anchored_text_size),
+                             loc=anchored_text_location)
+ax.add_artist(anchored_text)
 
+# Invert y-axis
+plt.gca().invert_yaxis()
+# Adjust for colorbar space
 f.subplots_adjust(right=0.8)
 cbar_ax = f.add_axes( (0.85, 0.15, 0.05, 0.7) )
+# Add colorbar and set tick size
 cbar = f.colorbar(c, cax=cbar_ax)
-# Set colorbar label
-cbar.ax.set_title(r"$\delta U(k,k')$")
+cbar.ax.tick_params(labelsize=colorbar_tick_size)
 
 plt.show()
-
-# Do log-scale
-
-mxx = 1e-1
-mnn = 1e-5
-color_style_log = 'Blues'
-
-f, ax = plt.subplots(1, 1, figsize=(4, 3.5))
-if channel == '1P1' or channel == '1S0':
-    c = ax.pcolormesh(k_array, k_array, abs(delta_U), cmap=color_style_log, 
-                      norm=colors.LogNorm(vmin=mnn, vmax=mxx))
-elif channel == '3S1':
-    c = ax.pcolormesh(k_array, k_array, abs(delta_U[:ntot,:ntot]), cmap=color_style_log, 
-                      norm=colors.LogNorm(vmin=mnn, vmax=mxx))
-ax.xaxis.set_label_position('top')
-ax.xaxis.tick_top()
-ax.tick_params(labeltop=True)
-ax.set_xlabel(x_label)
-ax.set_ylabel(y_label)
-channel_anchored_text = AnchoredText(ff.channel_label_conversion(channel),
-                                     loc='lower right')
-ax.add_artist(channel_anchored_text)
-plt.gca().invert_yaxis()
-
-f.subplots_adjust(right=0.8)
-cbar_ax = f.add_axes( (0.85, 0.15, 0.05, 0.7) )
-cbar = f.colorbar(c, cax=cbar_ax)
-# Set colorbar label
-cbar.ax.set_title(r"$\delta U(k,k')$")
-
-plt.show()
-
-# Load wave function
-psi = ob.wave_function(H_initial)
-#k_index = op.find_q_index(3.00, k_array)
-#print(psi[k_index])
