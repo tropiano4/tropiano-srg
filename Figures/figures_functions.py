ff3b02d0743944f3c657adfbbe6e63afee3f9a7a
tropiano4 <tropiano.4@osu.edu>
2020-06-29 12:28:04 -0400

    Update figures_functions.py
    Minor updates mostly to the docstrings.

--- Figures/figures_functions.py
+++ Figures/figures_functions.py
@@ -25,9 +25,8 @@
 #                block-diagonal \Lambda_BD.
 #   06/03/20 --- Made vector interpolation function analogous to
 #                interpolate_matrix function.
-#   06/25/20 --- Added a couple more linestyles to line_styles function.
-#                Updated replace_periods_with_commas to
-#                replace_periods.
+#   06/25/20 --- Added a couple more linestyles to line_styles function and
+#                updated replace_periods_with_commas to replace_periods.
 #
 #------------------------------------------------------------------------------
 
@@ -43,7 +42,7 @@ def channel_label_conversion(channel):
     Parameters
     ----------
     channel : str
-        The partial wave channel ('1S0', '3S1', etc.)
+        The partial wave channel (e.g. '1S0').
         
     Returns
     -------
@@ -51,7 +50,7 @@ def channel_label_conversion(channel):
         Label for the channel.
         
     """
-    
+
     # Make numbers super- and sub-scripts
     label = r'$^%s$' % channel[0] + channel[1] + r'$_%s$' % channel[2]
     
@@ -79,80 +78,32 @@ def generator_label_conversion(generator, lambda_bd=0.00):
 
     # Block-diagonal
     if generator == 'Block-diag':
-        # In the case, lambda_bd is not entered as an argument, return just
-        # G = H_BD
+        
+        # In the case, lambda_bd is not entered as an argument, the function
+        # returns only G = H_{BD}
         if lambda_bd == 0.00:
             label = r'$G=H_{BD}$'
+            
         # If lambda_bd is already an integer, format as an integer
         elif int(lambda_bd) == lambda_bd:
             label = r'$G=H_{BD}$' + ' (%d fm' % lambda_bd + r'$^{-1}$' + ')'
+            
         # Otherwise, present with two decimal places
         else:
             label = r'$G=H_{BD}$' + ' (%.2f fm' % lambda_bd + r'$^{-1}$' + ')'
+            
     elif generator == 'Wegner':
         label = r'$G=H_{D}$'
+        
     elif generator == 'T':
         label = r'$G=T_{rel}$'
 
     return label
 
 
-def interpolate_vector(x_array, y_array, x_max, ntot=500, order='linear'):
-    """
-    Interpolate vector given array for plots. Also adds more points to given
-    x_array.
-    
-    Parameters
-    ----------
-    x_array : 1-D ndarray
-        Array of x points where the matrix M = M(x, x).
-    y_array : 1-D ndarray
-        Vector to be interpolated.
-    x_max : float
-        Maximum value of x. This functions interpolates y_array up to this value.
-    ntot : int, optional
-        Desired length of the interpolated vector and new x array.
-    order : str, optional
-        Order for interpolation. Default is 'linear'.
-        
-    Returns
-    -------
-    x_array_new : 1-D ndarray
-        Array of ntot x points.
-    y_array_new : 1-D ndarray
-        Interpolated vector.
-        
-    """
-    
-    # This is a tricky way to select all the x points in the given array that 
-    # are within the specified range of 0 to x_max
-    
-    # List of boolean values (True or False)
-    bool_list = x_array <= x_max 
-    
-    # The number of points in the given momentum array less than k_max
-    n = len( list( filter(None, bool_list) ) )
-    
-    # Resize x_array and M to size that you want to plot
-    x_array = x_array[:n]
-    y_array = y_array[:n]
-    
-    # Use interp2d to interpolate the truncated matrix
-    y_func = interp1d(x_array, y_array, kind=order)
-
-    # New x array for interpolation (dimension ntot x 1)
-    x_array_new = np.linspace(x_array[0], x_array[-1], ntot)
-    
-    # New y array (dimension ntot x 1)
-    y_array_new = y_func(x_array_new) 
-    
-    return x_array_new, y_array_new
-
-
 def interpolate_matrix(x_array, M, x_max, ntot=500):
     """
-    Interpolate matrix over given array for contour plots. Also adds more
-    points to given x_array.
+    Interpolate matrix over given array for contour plots.
     
     Parameters
     ----------
@@ -177,6 +128,63 @@ def interpolate_matrix(x_array, M, x_max, ntot=500):
     # This is a tricky way to select all the x points in the given array that 
     # are within the specified range of 0 to x_max
     
+    # List of boolean values
+    bool_list = x_array <= x_max 
+    
+    # The number of points in the given momentum array less than x_max
+    n = len( list( filter(None, bool_list) ) )
+    
+    # Resize x_array and M to size that you want to plot
+    x_array_resized = x_array[:n]
+    M_resized = M[:n, :n]
+    
+    # Use interp2d to interpolate the truncated matrix
+    M_func = interp2d(x_array_resized, x_array_resized, M_resized)
+
+    # New x array for interpolation (dimension ntot x 1)
+    x_array_new = np.linspace(x_array_resized[0], x_array_resized[-1], ntot)
+    
+    # New matrix (dimension ntot x ntot)
+    M_new = M_func(x_array_new, x_array_new) 
+    
+    # We comment out the RectBivariateSpline method in what follows
+    #M_func = RectBivariateSpline(x_array_resized, x_array_resized, M_resized)
+    #col, row = np.meshgrid(x_array_new, x_array_new)      
+    #M_new = M_func.ev(row, col) # Spline
+    
+    return x_array_new, M_new
+
+
+def interpolate_vector(x_array, y_array, x_max, ntot=500, order='linear'):
+    """
+    Interpolate vector given array for plots.
+    
+    Parameters
+    ----------
+    x_array : 1-D ndarray
+        Array of x points where the vector y_array = y_array(x).
+    y_array : 1-D ndarray
+        Vector to be interpolated.
+    x_max : float
+        Maximum value of x. This functions interpolates y_array up to this 
+        value.
+    ntot : int, optional
+        Desired length of the interpolated vector and new x array.
+    order : str, optional
+        Order for interpolation. Default is 'linear'.
+        
+    Returns
+    -------
+    x_array_new : 1-D ndarray
+        Array of ntot x points.
+    y_array_new : 1-D ndarray
+        Interpolated vector.
+        
+    """
+    
+    # This is a tricky way to select all the x points in the given array that 
+    # are within the specified range of 0 to x_max
+    
     # List of boolean values (True or False)
     bool_list = x_array <= x_max 
     
@@ -184,24 +192,19 @@ def interpolate_matrix(x_array, M, x_max, ntot=500):
     n = len( list( filter(None, bool_list) ) )
     
     # Resize x_array and M to size that you want to plot
-    x_array = x_array[:n]
-    M = M[:n, :n]
+    x_array_resized = x_array[:n]
+    y_array_resized = y_array[:n]
     
-    # Use interp2d to interpolate the truncated matrix
-    M_func = interp2d(x_array, x_array, M)
+    # Use interp1d to interpolate the truncated vector
+    y_func = interp1d(x_array_resized, y_array_resized, kind=order)
 
     # New x array for interpolation (dimension ntot x 1)
-    x_array_new = np.linspace(x_array[0], x_array[-1], ntot)
+    x_array_new = np.linspace(x_array_resized[0], x_array_resized[-1], ntot)
     
-    # New matrix (dimension ntot x ntot)
-    M_new = M_func(x_array_new, x_array_new) 
+    # New y array (dimension ntot x 1)
+    y_array_new = y_func(x_array_new) 
     
-    # We comment out the RectBivariateSpline method in what follows
-    #M_func = RectBivariateSpline(x_array, x_array, M)
-    #col_mesh, row_mesh = np.meshgrid(x_array_new, x_array_new)      
-    #M_new = M_func.ev(row_mesh,col_mesh) # Spline
-    
-    return x_array_new, M_new
+    return x_array_new, y_array_new
 
 
 def kvnn_label_conversion(kvnn, full_label=True):
@@ -223,6 +226,11 @@ def kvnn_label_conversion(kvnn, full_label=True):
     label : str
         Label for the potential.
         
+    Notes
+    -----
+    It might be worth adding the cutoff to the full_label version for all the
+    potentials (e.g. EM N3LO 500 MeV).
+        
     """
 
     # Argonne v18
@@ -240,11 +248,11 @@ def kvnn_label_conversion(kvnn, full_label=True):
     # RKE N3LO (400, 450, 500 MeV cutoffs)
     elif kvnn in [105, 106, 107]:
         label = 'RKE N' + r'$^3$' + 'LO'
-    # RKE N4LO (400, 450, 500 MeV cutoffs)
+    # RKE N4LO (400, 450, 500, 550 MeV cutoffs)
     elif kvnn in [110, 111, 112, 113]:
         label = 'RKE N' + r'$^4$' + 'LO'
         
-    # Gezrelis N2LO (1 and 1.2 fm cutoff)
+    # Gezerlis N2LO (1 and 1.2 fm cutoff)
     elif kvnn in [222, 224]:
         label = 'Gezerlis N' + r'$^2$' + 'LO'
         
@@ -268,7 +276,7 @@ def kvnn_label_conversion(kvnn, full_label=True):
     return label
 
 
-def lambda_label_conversion(lamb, block_diag_bool=False):
+def lambda_label_conversion(lamb, generator='Wegner'):
     """
     Converts a lambda evolution parameter to a label for plotting purposes 
     (e.g. lamb = 2 gives r'$\lambda=2.0 fm^-1$').
@@ -277,8 +285,9 @@ def lambda_label_conversion(lamb, block_diag_bool=False):
     ----------
     lamb : float
         Evolution parameter lambda [fm^-1].
-    block_diag_bool : bool, optional
-        Determines whether lambda is referring to \lambda or \Lambda_BD.
+    generator : str, optional
+        SRG generator 'Wegner', 'T', or 'Block-diag'. Determines whether
+        lambda is referring to \lambda or \Lambda_BD.
         
     Returns
     -------
@@ -289,17 +298,22 @@ def lambda_label_conversion(lamb, block_diag_bool=False):
     
     # Case for lamb = infinity
     if lamb == np.inf:
+        
         # Label \lambda_bd
-        if block_diag_bool:
+        if generator == 'Block-diag':
             label = r'$\Lambda_{BD}=\infty$' + ' fm' + r'$^{-1}$'
+            
         # Label \lambda
         else:
             label = r'$\lambda=\infty$' + ' fm' + r'$^{-1}$'
+            
     # Case for finite lamb
     else:
+        
         # Label \lambda_bd
-        if block_diag_bool:
+        if generator == 'Block-diag':
             label = r'$\Lambda_{BD}=%.1f$' % lamb + ' fm' + r'$^{-1}$'
+            
         # Label \lambda
         else:
             label = r'$\lambda=%.1f$' % lamb + ' fm' + r'$^{-1}$'
@@ -309,16 +323,16 @@ def lambda_label_conversion(lamb, block_diag_bool=False):
 
 def line_styles(curve_number):
     """
-    Default curve line style for plotting. Styles are ordered by the ordering
-    of curves on the plot, that is, the first curve corresponds
-    to 'solid', the second to 'dashdot', etc. Do not use this function for
-    plots with more than four curves. An error message will display when a
-    number higher than four is used and the function will return 'solid'.
+    Default line styles for plotting curves. Styles are set by the ordering
+    of curves on the plot, that is, the first curve corresponds to 'solid', 
+    the second to 'dashdot', etc. Do not use this function for plots with more
+    than six curves. An error message will display when a number higher than
+    six is used and the function will return 'solid'.
     
     Parameters
     ----------
     curve_number : int
-        The number of curves being assigned a color.
+        The curve number being assigned a line style.
     
     Returns
     -------
@@ -332,7 +346,7 @@ def line_styles(curve_number):
     
     """
     
-    # Note, the last two are 'densely dashed' and densely dashdotted'
+    # Note, the last two are 'densely dashed' and 'densely dashdotted'
     line_styles = ['solid', 'dashdot', 'dashed', 'dotted', (0, (5, 1)), 
                    (0, (3, 1, 1, 1))]
     
@@ -374,7 +388,7 @@ def replace_periods(file_name):
     new_file_name = ''
     
     # Loop over each character in the original file name and append to the new
-    # file name replacing periods with commas
+    # file name replacing periods with '_'
     for letter in file_name:
         
         if letter == '.':
@@ -388,16 +402,16 @@ def replace_periods(file_name):
 
 def xkcd_colors(curve_number):
     """
-    Default curve colors for plotting using xkcd colors. Colors are ordered
-    by the ordering of curves on the plot, that is, the first curve corresponds
-    to black, the second to red, etc. Do not use this function for plots with
+    Default curve colors for plotting using xkcd colors. Colors are set by the
+    ordering of curves on the plot, that is, the first curve corresponds to
+    black, the second to red, etc. Do not use this function for plots with
     more than seven curves. An error message will display when a number higher
     than seven is used and the function will return 'xkcd:black'.
     
     Parameters
     ----------
     curve_number : int
-        The number of curves being assigned a color.
+        The curve number being assigned a color.
     
     Returns
     -------
