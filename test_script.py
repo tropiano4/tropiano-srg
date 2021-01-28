831039e6293663fbd9bdd1bca22772b5169f469d
tropiano4 <tropiano.4@osu.edu>
2021-01-28 13:01:43 -0500

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -34,178 +34,148 @@
 
 
 # Description of this test:
-#   Testing an automatic way of selecting relevant quantum numbers for SRC
-#    work (e.g., given m_s, m_s', m_t, m_t' what are the allowed S, T, etc.?)
+#   Calculate n_{\lambda}(q, Q=0) for different nuclei using LDA. Split into
+#   pp, pn, nn, np contributions as in Ryckebusch_2019oya.
 
 
 import numpy as np
+# Scripts made by A.T.
+from operators import find_q_index
+from Potentials.vsrg_macos import vnn
+from SRG_codes.srg_unitary_transformation import SRG_unitary_transformation
 
 
-def total_isospin(m_t, m_tp):
+def n_lambda_pair(pair, q, kvnn, lamb):
     """
-    Returns the possible values of T and M_T given m_t and m_tp.
+    Gives the N,Np contribution to the SRG-evolved pair momentum distribution
+    under the following approximations:
+        1. Q_vector = 0.
+        2. q >> k_F.
+        3. Taking only S-waves: 1S0, 3S1-3S1, 3S1-3D1.
     
     Parameters
     ----------
-    m_t : float
-        Isospin projection of particle 1. Must be 1/2 or -1/2.
-    m_tp : float
-        Isospin projection of particle 2. Must be 1/2 or -1/2.
-        
+    pair : str
+        Type of pair: 'pp', 'pn', 'nn', or 'np'.
+    q : float
+        High momentum value [fm^-1].
+    kvnn : int
+        This number specifies the potential.
+    lamb : float
+        Evolution parameter lambda [fm^-1].
+
     Returns
     -------
-    T_array : 1-D ndarray
-        Possible values of total isospin.
-    M_T : int
-        Total isospin projection.
-        
+    XXXX FINISH THIS PART XXXXX
+    
+    Notes
+    -----
+    1. Make functions for each factor with arguments T, M_T, etc.
+    2. Should lamb be replaced by k_F as an argument instead?
+    3. Should it be (U_matrix - I) / row / col or U_matrix / row / col - I?
+    
+
     """
     
-    M_T = int( m_t + m_tp )
+    # Set T, M_T, S, etc. based on the entered pair ('pp', 'pn', etc.)'
+    if pair == 'pp' or pair == 'nn':
+        
+        # T = 1
+        # M_T = 1 or M_T = -1 (doesn't matter!)
+        # Make function that takes T, M_T as argument?
+        # Clebsch-Gordan coefficients for isospin are 1 here
+        # < 1/2 1/2 | 1 1 > = 1 or < -1/2 -1/2 | 1 1 > = 1
+        isospin_factor = 1
+        
+        # Select the allowed value of S and M_S
+        # S = 0
+        # M_S = 0
+        # Make function that takes S, M_S as argument?
+        
+        # We sum over m_s, m_s', m_s_1, m_s_2 restricted to the following
+        # values:
+        #   m_s = up, m_s' = down, m_s_1 = up, m_s_2 = down
+        #   m_s = up, m_s' = down, m_s_1 = down, m_s_2 = up
+        #   m_s = down, m_s' = up, m_s_1 = up, m_s_2 = down
+        #   m_s = down, m_s' = up, m_s_1 = down, m_s_2 = up
+        # Each time we get factors of 1/\sqrt(2) or -1/\sqrt(2) but
+        # appearing in even powers - thus, it's always a factor of 1/4.
+        # But there are 4 possible combinations and we are summing over them!
+        spin_factor = 1
+        
+        # Select the associated partial wave channel
+        channel = '1S0'
+        # This means L=0, M_L=0, J=0, M_J=0
+        # < M_L M_S | J M_J> factors are always 1 in this case
+        total_ang_momentum_factor = 1
+        # Is this ever not 1?
+        
+        # Expansion of k_vector and q_vector factors
+        k_expansion_factor = 1 / (2*np.pi**2) # ( \sqrt(2/\pi)*Y00 )^2
+        q_expansion_factor = 1 / (2*np.pi**2) # ( \sqrt(2/\pi)*Y00 )^2
+        
+        # Let's combine all the relevant factors here to be concise
+        factor = isospin_factor * spin_factor * total_ang_momentum_factor * \
+                 k_expansion_factor * q_expansion_factor
+        
+        # Load Hamiltonian and calculate \delta U(k, q)
+        k_array, k_weights = vnn.load_momentum(kvnn, channel)
+        # Length of k_array
+        ntot = len(k_array)
+        factor_array = k_array * np.sqrt( 2*k_weights / np.pi )
+        row, col = np.meshgrid(factor_array, factor_array)
+        
+        H_initial = vnn.load_hamiltonian(kvnn, channel) # MeV
+        # Load band-diagonal-evolved Hamiltonian
+        H_evolved = vnn.load_hamiltonian(kvnn, channel, method='srg', 
+                                         generator='Wegner', lamb=lamb) # MeV
+        
+        # Calculate SRG transformation
+        U_matrix = SRG_unitary_transformation(H_initial, H_evolved)
+        
+        # Subtract out identity matrix and divide out momentum and weights
+        I = np.eye(ntot, ntot)
+        delta_U_matrix = (U_matrix - I) / row / col
+        
+        # Now find the matrix element of delta_U(k, q)
+        q_index = find_q_index(q, k_array)
+        delta_U_vector = delta_U_matrix[:ntot, q_index]
+        # Do the same for delta_U^{\dagger}
+        delta_U_dag_vector = delta_U_matrix.T[q_index, :ntot]
+        
+        # The overall contribution to the evolved pair momentum distribution
+        # as a function of k (which is summed to k_F later)
+        pair_contribution_k = delta_U_vector * delta_U_dag_vector * factor
+        # 1/(2*\pi^2)^2 * \delta U_1S0(k,q) * \delta U_1S0^{\dagger}(q,k)
+                              
+
+        # should have a vector that depends on k values
+
+
+
+
+    # Worry about this later   
+    elif pair == 'pn' or pair == 'np':
+        
+        T_values = [0, 1]
+        
+        # Loop over T_values and calculate isospin coefficients
     
-    if abs(M_T) == 1:
-        T_array = np.array( [1] )
+        # Sum over each spin projection
+    
+    
+        # Select the associated partial wave channel
+        
+        pair_contribution_k = 0
+
+        # should have a vector that depends on k values
+        
     else:
-        T_array = np.array( [0, 1] )
-    
-    return T_array, M_T
-
-
-def total_spin(m_s, m_sp, T):
-    """
-    Returns the possible values of S and M_S given m_s, m_sp, and T.
-    
-    Parameters
-    ----------
-    m_s : float
-        Spin projection of particle 1. Must be 1/2 or -1/2.
-    m_sp : float
-        Spin projection of particle 2. Must be 1/2 or -1/2.
-    T : int
-        Total isospin projection.
         
-    Returns
-    -------
-    S : int
-        Total spin.
-    M_S : int
-        Total spin projection.
-        
-    """
-    
-    M_S = int( m_s + m_sp )
-    
-    if T == 0:
-        S = 1
-    else:
-        S = 0
-        
-    if abs(M_S) == 1 and T == 0:
-        S = 1
-    elif abs(M_S) == 1 and T == 1:
-        S = None # Not allowed
-    elif M_S == 0 and T == 0:
-        S = 1
-    else:
-        S = 0
-        
-    return S, M_S
+        print("Enter a valid pair: 'pp', 'pn', 'nn', or 'np'.")
+        return None
 
-
-def orbital_angular_momentum(S):
-    """
-    Returns the possible orbital angular momentum values excluding 3+.
-
-    Parameters
-    ----------
-    S : int
-        Total spin.
-
-    Returns
-    -------
-    L_array : 1-D ndarray
-        Possible values of orbital angular momentum.
-
-    """
-    
-    if S == 0:
-        L_array = np.array( [0] ) # Only 1S0
-    else:
-        L_array = np.array( [0, 2] ) # S=1 -> 3S1 - 3D1 coupled channel
-        
-    return L_array
-        
-
-def orbital_angular_momentum_projection(L):
-    """
-    Returns possible orbital angular momentum projections.
-
-    Parameters
-    ----------
-    L : int
-        Orbital angular momentum.
-
-    Returns
-    -------
-    M_L_array : 1-D ndarray
-        Possible values of orbital angular momentum projection.
-
-    """
-    
-    if L == 0:
-        M_L_array = np.array( [0] )
-    else: # L=2
-        M_L_array = np.array( [-2, -1, 0, 1, 2] )
-        
-    return M_L_array
-
-
-def total_angular_momentum(M_L, M_S):
-    """
-    Returns the possible values for 
-
-    Parameters
-    ----------
-    M_L : TYPE
-        DESCRIPTION.
-    M_S : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
-    """
+    # Sum over k up to k_F
+    # This is where nuclear averaging is done (read on LDA)
     
     return None
-
-
-
-if __name__ == '__main__':
-    
-    particle_spins = np.array( [-0.5, 0.5] )
-    
-    for m_t in particle_spins:
-        for m_tp in particle_spins:
-            
-            print('_'*50)
-            print( 'm_t = %.1f, m_tp = %.1f' % (m_t, m_tp) )
-            T_array, M_T = total_isospin(m_t, m_tp)
-            
-            for T in T_array:
-                
-                print( 'T = %d, M_T = %d' % (T, M_T) )
-                print('-'*50)
-                
-                for m_s in particle_spins:
-                    for m_sp in particle_spins:
-                        
-                        print( 'm_s = %.1f, m_sp = %.1f' % (m_s, m_sp) )
-                        S, M_S = total_spin(m_s, m_sp, T)
-                        if S == 1 or S == 0:
-                            print( 'S = %d, M_S = %d\n' % (S, M_S) )
-                        else:
-                            print('Not allowed.\n')
-                            
-                print('-'*50)
-                
