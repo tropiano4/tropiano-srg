4bf4ae84eec7c9edd1c30d3da1b697be78d15b45
Anthony Tropiano <tropiano_anthony@yahoo.com>
2025-06-16 10:51:46 -0500

    Testing Frobenius norm of current operator

--- code/scratchwork.py
+++ code/scratchwork.py
@@ -12,12 +12,12 @@ from scripts.potentials import Potential
 from scripts.srg import SRG
 
 
-# kvnn, kmax, kmid, ntot = 6, 25.0, 4.0, 120
-kvnn, kmax, kmid, ntot = 7, 15.0, 3.0, 120
+kvnn, kmax, kmid, ntot = 6, 25.0, 4.0, 120
+# kvnn, kmax, kmid, ntot = 7, 15.0, 3.0, 120
 channels = ['1S0', '3S1', '3P0', '1P1', '3P1', '3P2', '1D2', '3D2', '3D3']
-# lambda_array = np.array([15.0])
+lambda_array = np.array([6.0])
 # channels = ['3F3', '1F3', '3F4']
-lambda_array = np.array([4.0, 3.0, 2.0, 1.5, 1.2])
+# lambda_array = np.array([4.0, 3.0, 2.0, 1.5, 1.2])
 
 for channel in channels:
     
